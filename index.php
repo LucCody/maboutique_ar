95f6064c059b95a9c3abd426875cd591ffbba07e
Luciano Razafindrapeno <134954223+LucCody@users.noreply.github.com>
2026-03-21 14:47:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include('db.php');
 include('header.php');
 ?>
 <div style="text-align: center; padding: 50px 20px; background: white; border-radius: 15px; box-shadow: 0 4px 15px rgba(0,0,0,0.05);">
-    <h1>Bienvenue sur Ma Boutique Ar Madagascar</h1>
+    <h1>Bienvenue sur Expert Model Madagascar</h1>
     <p>Le meilleur des services numériques au prix juste.</p>
     <?php if(!isset($_SESSION['user_id'])): ?>
         <a href="login.php" class="btn" style="margin-top:20px;">🚀 Commencer maintenant</a>
