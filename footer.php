8bfa9ed85918bdbbe07b5972168517e8291a16f7
Luciano Razafindrapeno <134954223+LucCody@users.noreply.github.com>
2026-03-21 14:43:37 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div> <!-- Fin du container -->
 <footer style="text-align:center; margin-top:50px; padding:20px; color:gray; border-top:1px solid #eee;">
-    &copy; <?php echo date('Y'); ?> - Ma Boutique Ar - Madagascar
+    &copy; <?php echo date('Y'); ?> - Expert Model - Madagascar
 </footer>
 </body>
 </html>
